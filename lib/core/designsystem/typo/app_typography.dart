af90a5b6eeb973b4c3a84c1426ef913d0184bfc6
Atik Faysal <atikfaysal.jatri@gmail.com>
2025-03-23 15:49:49 +0600

    feat(home): working on home screen, visible component

--- lib/core/designsystem/typo/app_typography.dart
+++ lib/core/designsystem/typo/app_typography.dart
@@ -8,66 +8,86 @@ final class AppTypography extends ThemeExtension<AppTypography> with TextStyleFi
   final TextStyle titleLargeMedium;
   final TextStyle titleLargeSemiBold;
   final TextStyle titleLargeBold;
+  final TextStyle titleLargeLight;
 
   final TextStyle titleMediumRegular;
   final TextStyle titleMedium;
   final TextStyle titleMediumSemiBold;
   final TextStyle titleMediumBold;
+  final TextStyle titleMediumLight;
 
   final TextStyle titleSmallRegular;
   final TextStyle titleSmallMedium;
   final TextStyle titleSmallSemiBold;
   final TextStyle titleSmallBold;
+  final TextStyle titleSmallLight;
 
   final TextStyle bodyLargeRegular;
   final TextStyle bodyLargeMedium;
   final TextStyle bodyLargeSemiBold;
   final TextStyle bodyLargeBold;
+  final TextStyle bodyLargeLight;
 
   final TextStyle bodyMediumRegular;
   final TextStyle bodyMedium;
   final TextStyle bodyMediumSemiBold;
   final TextStyle bodyMediumBold;
+  final TextStyle bodyMediumLight;
 
   final TextStyle bodySmallRegular;
   final TextStyle bodySmallMedium;
   final TextStyle bodySmallSemiBold;
   final TextStyle bodySmallBold;
+  final TextStyle bodySmallLight;
 
   final TextStyle bodyExtraSmallRegular;
   final TextStyle bodyExtraSmallMedium;
   final TextStyle bodyExtraSmallSemiBold;
   final TextStyle bodyExtraSmallBold;
+  final TextStyle bodyExtraSmallLight;
 
   AppTypography._({
     required this.titleLargeRegular,
     required this.titleLargeMedium,
     required this.titleLargeSemiBold,
     required this.titleLargeBold,
+    required this.titleLargeLight,
+
     required this.titleMediumRegular,
     required this.titleMedium,
     required this.titleMediumSemiBold,
     required this.titleMediumBold,
+    required this.titleMediumLight,
+
     required this.titleSmallRegular,
     required this.titleSmallMedium,
     required this.titleSmallSemiBold,
     required this.titleSmallBold,
+    required this.titleSmallLight,
+
     required this.bodyLargeRegular,
     required this.bodyLargeMedium,
     required this.bodyLargeSemiBold,
     required this.bodyLargeBold,
+    required this.bodyLargeLight,
+
     required this.bodyMediumRegular,
     required this.bodyMedium,
     required this.bodyMediumSemiBold,
     required this.bodyMediumBold,
+    required this.bodyMediumLight,
+
     required this.bodySmallRegular,
     required this.bodySmallMedium,
     required this.bodySmallSemiBold,
     required this.bodySmallBold,
+    required this.bodySmallLight,
+
     required this.bodyExtraSmallRegular,
     required this.bodyExtraSmallMedium,
     required this.bodyExtraSmallSemiBold,
     required this.bodyExtraSmallBold,
+    required this.bodyExtraSmallLight,
   });
 
   @override
@@ -77,30 +97,43 @@ final class AppTypography extends ThemeExtension<AppTypography> with TextStyleFi
       titleLargeMedium: styles[1],
       titleLargeSemiBold: styles[2],
       titleLargeBold: styles[3],
-      titleMediumRegular: styles[4],
-      titleMedium: styles[5],
-      titleMediumSemiBold: styles[6],
-      titleMediumBold: styles[7],
-      titleSmallRegular: styles[8],
-      titleSmallMedium: styles[9],
-      titleSmallSemiBold: styles[10],
-      titleSmallBold: styles[11],
-      bodyLargeRegular: styles[12],
-      bodyLargeMedium: styles[13],
-      bodyLargeSemiBold: styles[14],
-      bodyLargeBold: styles[15],
-      bodyMediumRegular: styles[16],
-      bodyMedium: styles[17],
-      bodyMediumSemiBold: styles[18],
-      bodyMediumBold: styles[19],
-      bodySmallRegular: styles[20],
-      bodySmallMedium: styles[21],
-      bodySmallSemiBold: styles[22],
-      bodySmallBold: styles[23],
-      bodyExtraSmallRegular: styles[24],
-      bodyExtraSmallMedium: styles[25],
-      bodyExtraSmallSemiBold: styles[26],
-      bodyExtraSmallBold: styles[27],
+      titleLargeLight: styles[4],
+
+      titleMediumRegular: styles[5],
+      titleMedium: styles[6],
+      titleMediumSemiBold: styles[7],
+      titleMediumBold: styles[8],
+      titleMediumLight: styles[9],
+
+      titleSmallRegular: styles[10],
+      titleSmallMedium: styles[11],
+      titleSmallSemiBold: styles[12],
+      titleSmallBold: styles[13],
+      titleSmallLight: styles[14],
+
+      bodyLargeRegular: styles[15],
+      bodyLargeMedium: styles[16],
+      bodyLargeSemiBold: styles[17],
+      bodyLargeBold: styles[18],
+      bodyLargeLight: styles[19],
+
+      bodyMediumRegular: styles[20],
+      bodyMedium: styles[21],
+      bodyMediumSemiBold: styles[22],
+      bodyMediumBold: styles[23],
+      bodyMediumLight: styles[24],
+
+      bodySmallRegular: styles[25],
+      bodySmallMedium: styles[26],
+      bodySmallSemiBold: styles[27],
+      bodySmallBold: styles[28],
+      bodySmallLight: styles[29],
+
+      bodyExtraSmallRegular: styles[30],
+      bodyExtraSmallMedium: styles[31],
+      bodyExtraSmallSemiBold: styles[32],
+      bodyExtraSmallBold: styles[33],
+      bodyExtraSmallLight: styles[34],
     );
   }
 
@@ -164,30 +197,43 @@ final class AppTypography extends ThemeExtension<AppTypography> with TextStyleFi
     titleLargeMedium: _getTypoStyle(22, FontWeight.w500),
     titleLargeSemiBold: _getTypoStyle(22, FontWeight.w600),
     titleLargeBold: _getTypoStyle(22, FontWeight.w700),
+    titleLargeLight: _getTypoStyle(22, FontWeight.w300),
+
     titleMediumRegular: _getTypoStyle(20, FontWeight.w400),
     titleMedium: _getTypoStyle(20, FontWeight.w500),
     titleMediumSemiBold: _getTypoStyle(20, FontWeight.w600),
     titleMediumBold: _getTypoStyle(20, FontWeight.w700),
+    titleMediumLight: _getTypoStyle(20, FontWeight.w300),
+
     titleSmallRegular: _getTypoStyle(18, FontWeight.w400),
     titleSmallMedium: _getTypoStyle(18, FontWeight.w500),
     titleSmallSemiBold: _getTypoStyle(18, FontWeight.w600),
     titleSmallBold: _getTypoStyle(18, FontWeight.w700),
+    titleSmallLight: _getTypoStyle(18, FontWeight.w300),
+
     bodyLargeRegular: _getTypoStyle(16, FontWeight.w400),
     bodyLargeMedium: _getTypoStyle(16, FontWeight.w500),
     bodyLargeSemiBold: _getTypoStyle(16, FontWeight.w600),
     bodyLargeBold: _getTypoStyle(16, FontWeight.w700),
+    bodyLargeLight: _getTypoStyle(16, FontWeight.w300),
+
     bodyMediumRegular: _getTypoStyle(14, FontWeight.w400),
     bodyMedium: _getTypoStyle(14, FontWeight.w500),
     bodyMediumSemiBold: _getTypoStyle(14, FontWeight.w600),
     bodyMediumBold: _getTypoStyle(14, FontWeight.w700),
+    bodyMediumLight: _getTypoStyle(14, FontWeight.w300),
+
     bodySmallRegular: _getTypoStyle(12, FontWeight.w400),
     bodySmallMedium: _getTypoStyle(12, FontWeight.w500),
     bodySmallSemiBold: _getTypoStyle(12, FontWeight.w600),
     bodySmallBold: _getTypoStyle(12, FontWeight.w700),
+    bodySmallLight: _getTypoStyle(12, FontWeight.w300),
+
     bodyExtraSmallRegular: _getTypoStyle(11, FontWeight.w400),
     bodyExtraSmallMedium:  _getTypoStyle(11, FontWeight.w500),
     bodyExtraSmallSemiBold: _getTypoStyle(11, FontWeight.w600),
     bodyExtraSmallBold: _getTypoStyle(11, FontWeight.w700),
+    bodyExtraSmallLight: _getTypoStyle(11, FontWeight.w300),
   );
 
   static TextStyle _getTypoStyle(double fontSize, FontWeight fontWeight) {
--- lib/presentation/home/home_screen.dart
+++ lib/presentation/home/home_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter_component_playground/core/designsystem/extensions/theme_
 import 'package:flutter_component_playground/core/ui/widgets/common_appbar.dart';
 import 'package:flutter_component_playground/core/ui/widgets/scaffold_appbar.dart';
 import 'package:flutter_component_playground/localization/localize_extension.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -13,10 +14,29 @@ class HomeScreen extends StatelessWidget {
       appbar: CommonAppbar(title: context.getString.title_home),
       body: Column(
         children: [
-          Center(
-            child: Text(context.getString.title_home,  style: context.typography.bodyExtraSmallBold,),
+          Row(
+            children: [
+              Expanded(
+                child: _buildComponentItem(context, "Button"),
+              ),
+              Expanded(
+                child: _buildComponentItem(context, "Text Field"),
+              ),
+            ],
           ),
         ],
+      ),
+    );
+  }
+
+  Widget _buildComponentItem(BuildContext context, String componentName){
+    return Container(
+      height: 100.h,
+      padding: EdgeInsets.all(context.spacingSizes.small),
+      child: Card(
+        child: Center(
+          child: Text(componentName, style: context.typography.bodyMediumLight,),
+        ),
       ),
     );
   }
